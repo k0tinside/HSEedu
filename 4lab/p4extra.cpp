7a85725281418f1c741fedb28d327dfd2a28f91d
k0tinside <lenamihajlenko28234@gmail.com>
2025-12-06 20:11:18 +0300

    lab4 extra

---
+++ 4lab/p4extra.cpp
@@ -0,0 +1,98 @@
+#include <iostream>
+#include <windows.h>
+#include <iomanip>
+
+float dayAverage(float* row, int h) {
+    float summa = 0;
+    for (int j = 0; j < h; ++j) {
+        summa = summa + *(row + j);
+    }
+    return summa / h;
+}
+
+float overallAverage(float** p, int d, int h) {
+    float summa = 0;
+    for (int i = 0; i < d; ++i) {
+        for (int j = 0; j < h; ++j) {
+            summa = summa + *(*(p + i) + j);
+        }
+    }
+    return summa / (d * h);
+}
+
+typedef struct {
+    int num;
+    float temp;
+} days;
+
+days* hotDays(float** p, int d, int h, int& newCount) {
+    float avg = overallAverage(p, d, h);
+    for (int i = 0; i < d; ++i) {
+        if (dayAverage(*(p + i), h) > avg) {
+            newCount++;
+        }
+    }
+
+    days* pHotDays = new days[newCount];
+
+    int k = 0;
+
+    for (int i = 0; i < d; ++i) {
+        float dayAvg = dayAverage(*(p + i), h);
+        if (dayAvg > avg) {
+            (*(pHotDays + k)).num = i + 1;
+            (*(pHotDays + k)).temp = dayAvg;
+            k++;
+        }
+    }
+    return pHotDays;
+}
+
+int main() {
+    
+    SetConsoleOutputCP(65001);
+    SetConsoleCP(65001);
+    std::cout << std::fixed << std::setprecision(1);
+
+    int d, h;
+    
+    std::cout << "Введите количество дней: ";
+    std::cin >> d;
+
+    std::cout << "Введите количество измерений в дне: ";
+    std::cin >> h;
+
+    if (d <= 0 || h <= 0) {
+        std::cout << "Введено некорректное число измерений.";
+        return 0;
+    }
+
+    float** data = new float*[d];
+    for (int i = 0; i < d; ++i) {
+        *(data + i) = new float[h];
+        std::cout << "День " << i + 1 << ": ";
+        for (int j = 0; j < h; ++j) {
+            std::cin >> *(*(data + i) + j);
+        }
+    }
+    
+    std::cout << "\nОбщая средняя температура: " << overallAverage(data, d, h) << "\n";
+
+    int newCount = 0;
+    days* hotdays = hotDays(data, d, h, newCount);
+
+    std::cout << "Теплые дни:\n";
+
+    for (int i = 0; i < newCount; ++i) {
+        std::cout << "День " << (*(hotdays + i)).num << " (среднее = " << (*(hotdays + i)).temp << ")\n";
+    }
+
+    for (int i = 0; i < d; ++i) {
+        delete[] *(data + i);
+    }
+    delete[] data;
+
+    delete[] hotdays;
+
+    return 0;
+}
